cba07715e47b619d0cab90e90812dd7da5591e3c
jesus <jesusrc999@gmail.com>
2025-01-28 18:32:06 +0100

    add nodemon

--- routes/recipesRouter.js
+++ routes/recipesRouter.js
@@ -1,2 +1,18 @@
 const express = require('express');
-const { addUser, getAllUsers, delByNombre, updateUser, getUserById, getNameContent} = require("../controllers/userController");
+const { addRecipe, getAllRecipes, delByNombre, updateUser, getUserById, getNameContent} = require("../controllers/userController");
+// const { verifyToken, verifyAdmin } = require ( "../middleware/auth");
+const router = express.Router();
+
+
+
+
+// router.post("/users", addUser);
+// router.get("/users", getAllUsers);
+// router.delete("/users/:idUser", delByNombre);
+// router.patch("/users/:idUser", updateUser);
+// router.get("/users/myProfile", verifyToken, verifyAdmin , getUserById);
+// router.get("/usersNameContent/:name", getNameContent)
+
+// router.get();
+
+module.exports = router;
